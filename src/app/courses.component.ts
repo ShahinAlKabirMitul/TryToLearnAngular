816d4f0e2cda303b92c2948709c0f98147b15ad0
mitulbd <mitul.cse@gmail.com>
2017-09-06 23:15:06 +0600

    Angular Component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,7 +1,7 @@
 import{Component}from '@angular/core';
 @Component({
     selector:'courses',
-    template:'<h2>Coursesh</h2>'
+    template:'<h2>Courses</h2>'
 
 })
 export class CoursesComponent{
