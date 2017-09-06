62d59f54a75bc2bd1f317df4b0547a06afd5cbc9
mitulbd <mitul.cse@gmail.com>
2017-09-07 00:04:57 +0600

    Curses Service

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,21 @@
+import { CoursesService } from './courses.service';
 import{Component}from '@angular/core';
 @Component({
     selector:'courses',
-    template:'<h2>Courses</h2>'
+    template:`
+            <h2>{{title}}</h2>
+            <ul>
+                <li *ngFor="let c of courses">{{c}}</li>
+            </ul>
+            `
 
 })
 export class CoursesComponent{
+    title='List of Courses'
+    courses;
 
+    constructor(service:CoursesService){
+      //  let service=new CoursesService();
+        this.courses=service.getCourses();
+    }
 }
