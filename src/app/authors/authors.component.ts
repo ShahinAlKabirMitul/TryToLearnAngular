d88e8a0054c7dded03b26eb6c8e5d289fd1c2e49
mitulbd <mitul.cse@gmail.com>
2017-09-07 07:44:18 +0600

    add bootstrap  to project  npm install bootstrap --save

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -2,7 +2,7 @@ import { AuauthorsService } from './../auauthors.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-authors',
+  selector: 'authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
