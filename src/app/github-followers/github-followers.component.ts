b606f74d81a1f36069d69f67835aeb52a956491d
mitulbd <mitul.cse@gmail.com>
2017-09-22 11:15:46 +0600

    multiple Observable

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,9 @@
+
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import {Observable} from  'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'github-followers',
@@ -8,11 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+       private route:ActivatedRoute,
+       private service: GithubFollowersService)
+     { }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+     ] )
+     .subscribe(combine=>{
+     
+      let page=combine[1].get('page');
+    
+  
+      console.log(page);
+
     this.service.getAll()
     .subscribe(followers => this.followers = followers);
+     });
+     
+    
+
   }
 
 }
